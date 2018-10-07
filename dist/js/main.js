ebaf68b6a7655fc24fd777549298510ab11f6c12
Alex Hval <alexhval@juno.com>
2018-10-07 17:06:28 -0400

    fixed resume

--- dist/js/main.js
+++ dist/js/main.js
@@ -97,37 +97,3 @@ function init() {
   // Init TypeWriter
   new TypeWriter(txtElement, words, wait);
 }
-
-// Init Scroll Reveal
-window.sr = ScrollReveal();
-
-// Scroll Reveal Origins
-sr.reveal('#drop-down', {
-  duration: 2000,
-  origin: 'top',
-  distance: '300px'
-});
-
-sr.reveal('#about', {
-  duration: 2000,
-  origin: 'right',
-  distance: '1500px'
-});
-
-sr.reveal('#experience', {
-  duration: 2000,
-  origin: 'left',
-  distance: '1500px'
-});
-
-sr.reveal('#education', {
-  duration: 2000,
-  origin: 'right',
-  distance: '1500px'
-});
-
-sr.reveal('#references', {
-  duration: 2000,
-  origin: 'left',
-  distance: '1500px'
-});
---
+++ dist/js/resume.js
@@ -0,0 +1,33 @@
+// Init Scroll Reveal
+window.sr = ScrollReveal();
+
+// Scroll Reveal Origins
+sr.reveal('#drop-down', {
+  duration: 2000,
+  origin: 'top',
+  distance: '300px'
+});
+
+sr.reveal('#about', {
+  duration: 2000,
+  origin: 'right',
+  distance: '1500px'
+});
+
+sr.reveal('#experience', {
+  duration: 2000,
+  origin: 'left',
+  distance: '1500px'
+});
+
+sr.reveal('#education', {
+  duration: 2000,
+  origin: 'right',
+  distance: '1500px'
+});
+
+sr.reveal('#references', {
+  duration: 2000,
+  origin: 'left',
+  distance: '1500px'
+});
